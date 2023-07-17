d776a807c0399050a6c6c49ce32e78dd14c06c9a
Aman Thakur <taman8288@gmail.com>
2023-07-17 10:18:14 +0530

    first commit

---
+++ JavaScript Pratice/index.js
@@ -0,0 +1,35 @@
+// let a = 20;
+// let b = 20;
+// console.log(a+b)
+
+
+// let b = prompt("enter the value of a and b");
+// let b = -5;
+// if( b >=0){
+//     console.log("positive value");
+// }
+// else{
+//     console.log("nagitive value ");
+// }
+
+
+
+
+// let man = 0;
+// if(man <= 13){
+//     console.log(" under the age of children ")
+// }
+// else if(man <= 18){
+//     console.log(" the age of 18")
+// }
+// else{
+//     console.log(" adalt")
+// }
+
+
+//  task of vikram bhaiya to give me in coaching;
+  
+let a = 10;
+let b = 5;
+
+console.log( a + b)
